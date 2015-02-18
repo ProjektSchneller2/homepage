60c5efdd1a0f86813414111c944d9ca5ce12b238
mad90 <martin.brehmer@gmx.de>
2015-02-18 17:28:10 +0100

    Test, einbindung eines ersten Login Formulars

--- supercert.php
+++ supercert.php
@@ -1,3 +1,36 @@
 <?php
+
+
+//loginvariable, muss z. B. mit Session verbunden und mit einem weiteren if case abgefragt bzw. geändert werden - eingeloggt, ausgeloggt
+$login = 0;
+
+if ($login=0){
+echo "Willkommen bei Supercert";
+echo "Bitte wählen Sie aus";
+//direkte html Verlinkung test
+echo "<a href=\"./anmeldung.html\">Anmelden</a>";
+//noch keine Verlinkung
+echo "<a href=\"\">Sie sind noch nicht angemeldet? Registrieren Sie sich hier!</a>";
+
+}
+else {
+	echo "Ein Fehler ist aufgetreten";
+}
 include ("Homepage.html");
+
+
+//wird evtl. später gebraucht um Seite von dieser Seite aufzubauen
+/*
+$homepage = get_homepage_parts('./homepage.html');
+//Template Tags ersetzen
+foreach ($template as &$part) {
+	switch ($part) {
+		case 'title';
+		$part = title();
+		break;
+		
+	}
+}
+
+echo implode($homepage)*/
 ?>
