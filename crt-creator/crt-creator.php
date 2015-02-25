7e145ba485f0fc35eb8fa6308c13c008315acb9a
puchi- <andreas@puchinger.net>
2015-02-25 20:10:11 +0100

    bugfixing

--- crt-creator/crt-creator.php
+++ crt-creator/crt-creator.php
@@ -1,11 +1,12 @@
 <?php
 //Intermediate:
 //$dir.'/ca.csr= der Ort der CSR
-$command = "/usr/bin/openssl ca -config 'rootCA/openssl.cnf' -days ".$days." -notext -batch -in ".$dir.'/ca.csr' -extensionsv3_ca -out intermediate.crt;
+$command = "/usr/bin/openssl ca -config 'rootCA/openssl.cnf' -days ".$days." -notext -batch -in ".$dir."'/ca.csr' -extensionsv3_ca -out intermediate.crt";
 $output = shell_exec($command);
 
 //WC:
 //$dir.'/ca.csr= der Ort der CSR für WC
 // signieren wir WCs mit intermediate oder mit der rootCA??
-$command = "/usr/bin/openssl ca -config ' intermediate /openssl.cnf' -days ".$days." -notext -batch -in ".$dir.'/ca.csr' -out WC.crt;
+$command = "/usr/bin/openssl ca -config ' intermediate /openssl.cnf' -days ".$days." -notext -batch -in ".$dir."'/ca.csr' -out WC.crt";
 $output = shell_exec($command);
+?>
