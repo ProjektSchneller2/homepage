09f7fd787dc74c3c2fdc6fef108a5aa7b37f2b6d
puchi- <andreas@puchinger.net>
2015-02-26 12:47:53 +0100

    einbindung login

--- csrupload.php
+++ csrupload.php
@@ -1,4 +1,15 @@
 <?php
+session_start();
+?>
+
+<?php
+
+if(!isset($_SESSION["username"]))
+{
+	echo "Bitte erst <a href=\"anmeldung.html\">einloggen</a>";
+	exit;
+}
+
 foreach ($_FILES["csr"]["error"] as $key => $error) {
 	if ($error == UPLOAD_ERR_OK) {
 		$tmp_name = $_FILES["csr"]["tmp_name"][$key];
