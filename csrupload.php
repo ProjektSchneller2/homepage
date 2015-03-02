dd81d026e278b3a160bc0ba4f9268f715d5384cb
mad90 <martin.brehmer@gmx.de>
2015-03-02 17:02:22 +0100

    CSR upload funktioniert - ein paar schönheitskorrekturen müssen noch gemacht werden + Datenbankzugriff/Einspeisung von CSR Parameter zur Identifikation im Backend

--- csrupload.php
+++ csrupload.php
@@ -1,47 +1,54 @@
+<html>
 <form name="uploadformular" enctype="multipart/form-data" action="csrupload.php" method="post" >
-Bitte laden Sie hier Ihre signierte CSR Datei hoch: <input type="file" name="uploaddatei" size="60" maxlength="255" >
+Bitte laden Sie hier Ihre signierte CSR Datei hoch: 
+<p><input type="file" name="csruploadfile" size="60" maxlength="255" >
 <input type="Submit" name="csrupload" value="Datei hochladen">
+<p>
 </form>
-
+</html>
 <?php
-session_start();
 
 
-if(!isset($_SESSION["username"]))
-{
-	echo "Bitte erst <a href=\"anmeldung.html\">einloggen</a>";
-	exit;
-}
+if ($_FILES != Null){
+	session_start();
+
+	if(!isset($_SESSION["username"]))
+	{
+		echo "Bitte erst <a href=\"anmeldung.html\">einloggen</a>";
+		exit;
+	}
+	
 echo "<pre>";
 echo "FILES:<br />";
 print_r ($_FILES );
 echo "</pre>";
-if ( $_FILES['uploaddatei']['name']  != Null )
+
+var_dump($_POST);
+if ( $_FILES['csruploadfile']['name']  != Null )
 {
-	// Datei wurde durch HTML-Formular hochgeladen
-	// und kann nun weiterverarbeitet werden
+	//if($dateityp == "application/x-x509-ca-cert" 
+	//|| $dateityp == "application/octet-stream" 
+	//|| $dateityp == "application/pkcs10")
+	$zugelassenedateitypen = array("application/x-x509-ca-cert"|| "application/octet-stream"|| "application/pkcs10");
 
-	// Kontrolle, ob Dateityp zulässig ist
-	$zugelassenedateitypen = array("application/cert");
-
-	if ( ! in_array( $_FILES['uploaddatei']['type'] , $zugelassenedateitypen ))
+	if ( ! in_array( $_FILES['csruploadfile']['type'] , $zugelassenedateitypen ))
 	{
 		echo "<p>Dateitype ist NICHT zugelassen</p>";
 	}
 	else
 	{
 		move_uploaded_file (
-		$_FILES['uploaddatei']['tmp_name'] ,
-		'hochgeladenes/'. $_FILES['uploaddatei']['name'] );
+		$_FILES['csruploadfile']['tmp_name'] ,
+		$_SESSION['username'].'/'. $_FILES['csruploadfile']['name'] );
 
 		echo "<p>Hochladen war erfolgreich: ";
-		echo '<a href="hochgeladenes/'. $_FILES['uploaddatei']['name'] .'">';
-		echo 'hochgeladenes/'. $_FILES['uploaddatei']['name'];
+		echo '<a href="'.$_SESSION['username'].'/'. $_FILES['csruploadfile']['name'] .'">';
+		echo $_SESSION['username']. $_FILES['csruploadfile']['name'];
 		echo '</a>';
 	}
 }
 
- 
+} 
 
 //foreach ($_FILES["csr"]["error"] as $key => $error) {
 	//if ($error == UPLOAD_ERR_OK) {
--- selectcert.php
+++ selectcert.php
@@ -8,19 +8,25 @@ if(!isset($_SESSION["username"]))
 	exit;
 }
 
-if (isset($_POST('submit'))){
-if (isset($_POST('selectcert'))){
-	if (isset($_POST('singlecert'))){
-	// hier muss der zertifikatstyp in die session gespeichert werden um in der csrupload.php verarbeitet zu werden - Speicherung in den richtigen Ordner
+
+$selection = $_POST["selectcert"];
+var_dump($_POST);
+
+  
+if ($selection == "singlecert"){
 	$_SESSION['certtype']="singlecert";
-	}
-	if (isset($_POST('intermediate'))){
-		$_SESSION['certtype']="intermediate";
-	}
-	if (isset($_POST('wildcard'))){
-		$_SESSION['certtype']="wildcard";
-	}
-	header ('Location: ./csrupload.php');
+	// statt in der Session muss es hier in der Datenbank gespeichert werden
 }
+
+if ($selection =="intermediate") {
+	$_SESSION['certtype']="intermediate";
+	
+	
 }
+if ($selection =="wildcard"){
+	
+	$_SESSION['certtype']="wildcard";
+}
+include ("csrupload.php");
+
 ?>
