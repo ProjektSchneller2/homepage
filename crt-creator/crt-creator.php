ec9a30c1b0f111185bd5fb74e5637a29f2d2a94d
Vera <cintia.morrison@yahoo.de>
2015-03-03 21:03:44 +0100

    Name der fertigen Zertifikate angepasst (wie besprochen). Die verschiedenen Variablen müssen noch SINNVOLL befüllt werden.

--- crt-creator/crt-creator.php
+++ crt-creator/crt-creator.php
@@ -1,37 +1,68 @@
 <?php
+//welches Zertifikat soll es werden? Intermediate/Wildcard/Sub-Ca?
+$certificat_kind= "Wildcard";
+
 //Tage über irgendein Feld von der Webseite übertragen
 $days = "365";
 
+//Username
+$username = "ABC";
+
+//Timestamp
+$timestamp = "1234";
+
 //Name des Ordners des Users
-$dir = "";
+$userbox = "";
 
-//csr werden mit welchem Namen hochgeladen?		(momentan ca.csr)
-//selber Name für die CRT-Datei?				(momentan WC.crt)
+//wo liegt das config-File?
+$cnf_pfad = "";
 
-//config-File von quasi root, der vom Server verwenden, wo abgelegt?
+//evtl muss openssl in einem bestimmten Ordner aufgerufen werden -> Pfad zum Ordner noch einfügen
 
-//kann ich einfach openssl aufrufen oder muss ich dafür auch erst auf ein bestimmtes Verzeichnis
-
-
-//Intermediate:
-$command = "\usr\bin\openssl ca -config intermediate.cnf -days ";
-$command += $days;
-$command += " -in ";
-$command += $dir;
-$command += "\ca.csr -extensions v3_ca -out ";
-$command += $dir;
-$command += "\WC.crt";
-$output = shell_exec($command);
+//Name der csr-Datei muss momentan immer "ca.csr" lauten
 
 
-//WC:
-$command = "\usr\bin\openssl ca -config intermediate.cnf -days ";
-$command += $days;
-$command += " -in "; 
-$command += $dir;
-$command += "\ca.csr -out ";
-$command += $dir;
-$command += "\WC.crt";
-$output = shell_exec($command);
+
+
+
+if ($certificat_kind=="Intermediate"){
+	//Intermediate:
+	$command = "\usr\bin\openssl ca -config intermediate.cnf -days ";
+	$command += $days;
+	$command += " -in ";
+	$command += $userbox;
+	$command += "\ca.csr -extensions v3_ca -out ";
+	$command += $userbox+"\\"+$username+"_IM_"+$timestamp+".crt";
+
+	$output = shell_exec($command);
+}
+
+else if($certificat_kind=="Wildcard"){
+	//WC:
+	$command = "\usr\bin\openssl ca -config intermediate.cnf -days ";
+	$command += $days;
+	$command += " -in "; 
+	$command += $userbox;
+	$command += "\ca.csr -out ";
+	$command += $userbox+"\\"+$username+"_WC_"+$timestamp+".crt";
+
+	$output = shell_exec($command);
+}
+
+else if($certificat_kind=="Sub-Ca"){
+	//WC:
+	$command = "\usr\bin\openssl ca -config intermediate.cnf -days ";
+	$command += $days;
+	$command += " -in ";
+	$command += $userbox;
+	$command += "\ca.csr -out ";
+	$command += $userbox+"\\"+$username+"_S-Ca_"+$timestamp+".crt";
+
+	$output = shell_exec($command);
+}
+
+else {
+	echo "Zertifikatsart nicht erkannt. Bitte fragen sie ihr hilfreiches Administratorteam.";
+}
 
 ?>
