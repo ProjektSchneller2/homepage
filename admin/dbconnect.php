a01f2015e64552434ab1136a5e4d465fdb9fdeb8
puchi- <andreas@puchinger.net>
2015-03-10 15:44:48 +0100

    Login-Funktion Admin Part1

---
+++ admin/dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+//Verbindungsdaten zur Datenbank zum inkludieren
+
+
+
+$db = mysqli_connect("localhost", "root" , "", "dhbw_ca_admin")
+or die("Verbindung zur Datenbank konnte nicht hergestellt werden");
+
+
+?>
---
+++ admin/login.php
@@ -0,0 +1,55 @@
+<?php
+//Einlogdaten + PWHash
+session_start();
+?>
+
+<?php 
+include 'dbconnect.php';
+
+$username = $_POST["username"]; 
+$passwort = md5($_POST["password"]); 
+
+$abfrage = "SELECT username, passwort FROM login WHERE username LIKE '$username' LIMIT 1"; 
+$ergebnis = mysqli_query($db, $abfrage); 
+$row = mysqli_fetch_object($ergebnis); 
+
+if($row->passwort == $passwort) 
+    { 
+    $_SESSION["username"] = $username; 
+    echo "Login erfolgreich. <br> <a href=\"admin.php\">admin.php</a>"; 
+    } 
+else 
+    { 
+    echo "Benutzername und/oder Passwort waren falsch. <a href=\"anmeldung.html\">Login</a>"; 
+    } 
+
+
+
+/* old code:
+ * 
+ * if (isset($_POST['username'])) {
+	$username = $_POST['username'];
+} else {
+	$error = TRUE;
+}
+if (isset($_POST['password'])) {
+	$pw = hash("sha512", $_POST['password']);
+} else {
+	$error = TRUE;
+}
+
+$account = NULL;
+if ($db->get_admin($username) != NULL) {
+	$account = reset($db->get_admin($username));
+}
+
+if ($account['password'] == $pw) {
+	$_SESSION['accountaccept'] = TRUE;
+} else {
+	$_SESSION['accountaccept'] = FALSE;
+}
+
+header('Location: supercert.php');
+ */
+
+?>
