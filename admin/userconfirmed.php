50e7eead972c242f0734b1f036b1dda940cb5bc1
puchi- <andreas@puchinger.net>
2015-03-16 22:22:37 +0100

    bugfixing

--- admin/userconfirmed.php
+++ admin/userconfirmed.php
@@ -6,7 +6,7 @@ include '../dbconnect.php';
 $username = $_POST["username"];
 $abfrage = "UPDATE login SET freischaltung = 1 WHERE username = '$username'";
 $ergebnis = mysqli_query($db, $abfrage);
-mkdir ("../users/".$username, 0700);
+mkdir ("../users/".$username, 0775);
 header('Location: admin.php');
 
 ?>
