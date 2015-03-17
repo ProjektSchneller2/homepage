8cf704395ae516edb3b05037313b04cf7f769e32
ddmler <eddmler@googlemail.com>
2015-03-17 21:44:15 +0100

    Implementing Prepared Statement, Cleanup

--- login.php
+++ login.php
@@ -2,61 +2,32 @@
 //Einlogdaten + PWHash
 session_start();
 require_once 'header.php';
-echo "<div class=\"container\">";
-?>
-
-<?php 
+echo "<div class=\"container\">"; //@TODO: needed?
 include 'dbconnect.php';
 
 $username = $_POST["username"]; 
-$passwort = md5($_POST["password"]); 
+$passwort = md5($_POST["password"]); //@TODO: PHP 5.5 and above: change to $passwort = password_hash($passwort, PASSWORD_DEFAULT);
+									 //@TODO: set database column to length 255, delete all users, register new
 
-$abfrage = "SELECT username, passwort, freischaltung FROM login WHERE username LIKE '$username' LIMIT 1"; 
-$ergebnis = mysqli_query($db, $abfrage); 
-$row = mysqli_fetch_object($ergebnis); 
+//get database record for user
+$stmt = $mysqli->prepare("SELECT username, passwort, freischaltung FROM login WHERE username LIKE ? LIMIT 1");
+$stmt->bind_param("s", $username);
+$stmt->execute();
+$res = $stmt->get_result();
+$row = $res->fetch_object();
 
-if($row->freischaltung == 0){
-	echo "Ihr Account wurde noch nicht freigeschaltet. <a href=\"anmeldung.html\">Login</a>";
-}
-else {
-
-if($row->passwort == $passwort) 
-    { 
-    $_SESSION["username"] = $username; 
-    header('Location: supercert.php'); 
-    } 
-else 
-    { 
-    echo "Benutzername und/oder Passwort waren falsch. <a href=\"anmeldung.html\">Login</a>"; 
-    } 
-}
-
-
-/* old code:
- * 
- * if (isset($_POST['username'])) {
-	$username = $_POST['username'];
-} else {
-	$error = TRUE;
-}
-if (isset($_POST['password'])) {
-	$pw = hash("sha512", $_POST['password']);
-} else {
-	$error = TRUE;
+//is user unlocked
+if ($row->freischaltung == 1) {
+	//verify password
+	if ($row->passwort == $passwort) {//@TODO: if (password_verify($passwort, $row->passwort)) { Login success } 
+		$_SESSION["username"] = $username; 
+		header('Location: supercert.php'); 
+	} else { 
+		echo "Benutzername und/oder Passwort waren falsch. <a href=\"login.html\">Login</a>"; 
+	} 
+} else { //freischaltung != 1 -> 0 or something different (because of error in database type etc.)
+	echo "Ihr Account wurde noch nicht freigeschaltet. <a href=\"login.html\">Login</a>";
 }
 
-$account = NULL;
-if ($db->get_admin($username) != NULL) {
-	$account = reset($db->get_admin($username));
-}
-
-if ($account['password'] == $pw) {
-	$_SESSION['accountaccept'] = TRUE;
-} else {
-	$_SESSION['accountaccept'] = FALSE;
-}
-
-header('Location: supercert.php');
- */
 echo "</div>";
 ?>
