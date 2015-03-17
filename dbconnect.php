7b70fc34310d23d65dad1611251b230861fcfa03
Andreas Puchinger <andreas@puchinger.net>
2015-03-17 20:53:02 +0100

    Correction

--- dbconnect.php
+++ dbconnect.php
@@ -3,7 +3,7 @@
 
 
 
-$db = mysqli_connect("localhost", "dhbwca-user" , "UAV2z7AwWpUpa8Nj", "dhbwca_user")
+$db = mysqli_connect("localhost", "root" , "", "dhbw_ca_prod")
 or die("Verbindung zur Datenbank konnte nicht hergestellt werden");
 
 
