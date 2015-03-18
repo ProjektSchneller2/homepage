f5d4b2e6ea3b6810294de34ab49c64e7f57bda04
puchi- <andreas@puchinger.net>
2015-03-18 02:43:19 +0100

    bugfix mkdir+chmod

--- admin/userconfirmed.php
+++ admin/userconfirmed.php
@@ -6,7 +6,8 @@ include '../dbconnect.php';
 $username = $_POST["username"];
 $abfrage = "UPDATE login SET freischaltung = 1 WHERE username = '$username'";
 $ergebnis = mysqli_query($db, $abfrage);
-mkdir ("../users/".$username, 0775);
+mkdir ("../users/".$username);
+chmod ("../users/".$username, 0775);
 header('Location: admin.php');
 
 ?>
