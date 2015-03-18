235b4f940f7828ae7e64513e43d6a054b0e07ea4
Enrico <eddmler@googlemail.com>
2015-03-18 13:51:55 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,31 +1,27 @@
 <?php
-//Einlogdaten + PWHash
 require_once 'header.php';
-echo "<div class=\"container\">"; //@TODO: needed?
 
 $username = $_POST["username"]; 
-$passwort = md5($_POST["password"]); //@TODO: PHP 5.5 and above: change to $passwort = password_hash($passwort, PASSWORD_DEFAULT);
-									 //@TODO: set database column to length 255, delete all users, register new
+$passwort = $_POST["password"];
 
 //get database record for user
-$stmt = mysqli_prepare ($db, "SELECT username, passwort, freischaltung FROM login WHERE username LIKE ? LIMIT 1");
+$stmt = mysqli_prepare("SELECT username, passwort, freischaltung FROM login WHERE username LIKE ? LIMIT 1");
 mysqli_stmt_bind_param($stmt, "s", $username);
-mysqli_stmt_execute ($stmt);
-$res = mysqli_stmt_get_result ($stmt);
-$row = mysqli_fetch_array ($res);
+mysqli_stmt_execute($stmt);
+$res = mysqli_stmt_get_result($stmt);
+$row = mysqli_fetch_array($res);
 
 //is user unlocked
 if ($row["freischaltung"] == 1) {
 	//verify password
-	if ($row["passwort"] == $passwort) {//@TODO: if (password_verify($passwort, $row->passwort)) { Login success } 
+	if (password_verify($passwort, $row["passwort"])) {
 		$_SESSION["username"] = $username; 
 		header('Location: supercert.php'); 
 	} else { 
-		echo "Benutzername und/oder Passwort waren falsch. <a href=\"anmeldung.html\">Login</a>"; 
+		echo "<p class=\"bg-danger\">Benutzername und/oder Passwort waren falsch. <a href=\"anmeldung.html\">Login</a></p>"; 
 	} 
 } else { //freischaltung != 1 -> 0 or something different (because of error in database type etc.)
-	echo "Ihr Account wurde noch nicht freigeschaltet. <a href=\"anmeldung.html\">Login</a>";
+	echo "<p class=\"bg-danger\">Ihr Account wurde noch nicht freigeschaltet. <a href=\"anmeldung.html\">Login</a></p>";
 }
 
-echo "</div>";
 ?>
